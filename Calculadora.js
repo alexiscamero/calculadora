6b9a799823435724f64196206f047bc474033a5f
alexiscamero <alexiscamerohernandez@gmail.com>
2024-04-20 20:00:12 +0200

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,29 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+
+    porcentaje(num1, num2) {
+        return num1 * (num2 / 100);
+    }
+
+    cuadrado(num1) {
+        return num1 * num1;
+    }
+
+    raiz(num1) {
+        return Math.sqrt(num1);
+    }
+} 
